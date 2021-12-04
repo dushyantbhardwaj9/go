9b5c51638a4028b8c47018a17b6e40d0acecdbf8
Dushyant Bhardwaj <dubhardwaj@deloitte.com>
2021-12-05 02:24:39 +0530

    Strucutred 1-PrintPattern

---
+++ GKG/Mathematical/1-PrintPattern/go.mod
@@ -0,0 +1,3 @@
+module github.com/dushyantbhardwaj9/go/GKG/Mathematical/1-PrintPattern
+
+go 1.16
--- GKG/Mathematical/1-PrintPattern/main.go
+++ GKG/Mathematical/1-PrintPattern/main.go
@@ -1,6 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	n := 5
-	pattern.printPattern(n)
+	printPattern(n)
+}
+
+func printPattern(n int) {
+	for i := 1; i < (n + 1); i++ {
+		for j := n; j > 0; j-- {
+			for k := 0; k < i; k++ {
+				fmt.Printf("%d", j)
+			}
+		}
+		fmt.Println()
+	}
 }
